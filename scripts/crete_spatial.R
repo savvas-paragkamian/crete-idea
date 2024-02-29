3a57763265324fab353a68483c259541eb6ab87b
savvas-paragkamian <s.paragkamian@gmail.com>
2024-02-29 16:13:14 +0200

    figures for ena all samples

--- scripts/crete_spatial.R
+++ scripts/crete_spatial.R
@@ -11,7 +11,7 @@
 
 library(sf)
 library(tidyverse)
-
+library(RColorBrewer)
 # crete borders
 crete_shp <- sf::st_read("data/crete/crete.shp") 
 
@@ -57,6 +57,38 @@ metadata_wide_sf <- metadata_wide %>%
              remove=F,
              crs="WGS84")
 
+ena_sample_summary <- metadata_wide |>
+    distinct(ENA_RUN,ENA_STUDY, organism, environment_feature, environment_biome, GOLD_Ecosystem_Classification) |>
+    group_by(ENA_STUDY, organism) |>
+    summarise(n=n(),.groups="keep") |>
+    group_by(organism) |>
+    summarise(studies=n(), samples=sum(n)) |>
+    arrange(organism)
+
+ena_sample_summary$organism <- factor(ena_sample_summary$organism, levels = c(sort(unique(ena_sample_summary$organism), decreasing = TRUE)))
+
+ena_samples_bar <- ggplot() + 
+    geom_col(ena_sample_summary, mapping=aes(x=samples, y=organism)) +
+    theme_bw()+
+    scale_x_continuous(breaks=seq(0,200,25))+
+    xlab("Number of samples")+
+    theme(axis.text.x = element_text(face="bold",
+                                     size = 15),
+          axis.title.x=element_text(face="bold",size=18),
+          axis.ticks.y=element_blank(),axis.title.y=element_blank(),
+          axis.text.y=element_text(size=10, hjust=0, vjust=0)) +
+    theme(legend.position="bottom",
+            panel.border = element_blank(),
+            panel.grid.major = element_blank(), #remove major gridlines
+            panel.grid.minor = element_blank())
+
+ggsave("figures/ena_samples_bar.png",
+       plot=ena_samples_bar,
+       device="png",
+       height = 75,
+       width = 38,
+       units="cm")
+
 # filter the terrestrial metagenomes only
 crete_terrestrial_metagenome <- st_intersection(metadata_wide_sf, crete_shp) %>%
     filter(grepl("metagenome",organism), !grepl("marine|sponge|seawater|sediment",organism))
@@ -70,6 +102,7 @@ length(unique(crete_terrestrial_metagenome$project_name))
 # Maps
 getPalette = colorRampPalette(brewer.pal(9, "Set1"))
 colourCount = length(unique(crete_terrestrial_metagenome$organism))
+colourCount_all = length(unique(metadata_wide_sf$organism))
 
 
 g_base <- ggplot() +
@@ -87,7 +120,7 @@ g_ena <- g_base +
     scale_color_manual(values = getPalette(colourCount)) +
     theme(legend.position = 'bottom',legend.title=element_blank())
 
-ggsave(paste0("figures/map_ena_samples_crete.png",sep=""),
+ggsave(paste0("figures/map_ena_terrestrial_metagenome_samples_crete.png",sep=""),
        plot=g_ena, 
        height = 20, 
        width = 30,
@@ -95,3 +128,20 @@ ggsave(paste0("figures/map_ena_samples_crete.png",sep=""),
        units="cm",
        device="png")
 
+g_ena_all <- g_base +
+    geom_point(metadata_wide_sf,
+               mapping=aes(x=longitude,
+                           y=latitude,
+                           color=organism),
+               size=2,
+               alpha=0.7) +
+    scale_color_manual(values = getPalette(colourCount_all)) +
+    theme(legend.position = 'bottom',legend.title=element_blank())
+
+ggsave(paste0("figures/map_ena_all_samples_crete.png",sep=""),
+       plot=g_ena_all, 
+       height = 20, 
+       width = 30,
+       dpi = 300, 
+       units="cm",
+       device="png")
