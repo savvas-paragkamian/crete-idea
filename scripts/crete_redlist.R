0c62c48dabc4ac177f6bfb96ba30c8fdfb291f26
savvas-paragkamian <s.paragkamian@gmail.com>
2024-07-14 03:41:42 +0300

    iucn analysis for Crete done

--- scripts/crete_redlist.R
+++ scripts/crete_redlist.R
@@ -10,8 +10,11 @@
 library(tidyverse)
 library(RColorBrewer)
 library(ggpubr)
+library(sf)
 
 # Load data
+crete_shp <- sf::st_read("data/crete/crete.shp") 
+polygons <- read_sf("data/redlist_species_polygons/data_0.shp")
 
 habitats <- read_delim("data/redlist_species_data/habitats.csv", delim=",")
 
@@ -22,7 +25,11 @@ non_marine_habitats <- habitats |>
 simple_summary <- read_delim("data/redlist_species_data/simple_summary.csv", delim=",") |>
     filter(scientificName %in% unique(non_marine_habitats$scientificName))
 
-# summary
+threats <- read_delim("data/redlist_species_data/threats.csv", delim=",") |>
+    filter(scientificName %in% unique(non_marine_habitats$scientificName))
+
+
+# summary taxa
 phyla_taxa <- simple_summary |>
     group_by(phylumName) |>
     summarise(species=n())
@@ -34,3 +41,35 @@ phyla_taxa_redlist <- simple_summary |>
     left_join(phyla_taxa)
 
 write_delim(phyla_taxa_redlist, "results/iucn_phyla_taxa_redlist.tsv", delim="\t")
+
+# summary threats
+
+threats_summary <- threats |> 
+    group_by(name) |> 
+    summarise(n=n()) |>
+    arrange(desc(n))
+
+threats_summary_phyla <- threats |> 
+    left_join(simple_summary) |>
+    group_by(name,phylumName) |> 
+    summarise(n=n(), .groups="keep") |>
+    arrange(desc(n))
+
+# maps 
+
+g_base <- ggplot() +
+    geom_sf(crete_shp, mapping=aes()) +
+    coord_sf(crs="WGS84") +
+    theme_bw()
+
+g_iucn <- g_base +
+    geom_sf(polygons, mapping=aes(fill=SCI_NAME),alpha=0.5, size=0.1, show.legend=F)+
+    theme_bw()
+
+ggsave("figures/iucn_taxa_map.png",
+       g_iucn,
+       height = 15, 
+       width = 20,
+       dpi = 600, 
+       unit="cm",
+       device="png")
